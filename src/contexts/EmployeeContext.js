fa8c6b0020c7fd17d8d6788cb7b5dfca8939187a
OMRFRKYL <omeryolu16@gmail.com>
2022-10-20 15:57:47 +0300

    LocaleStorage added.

--- src/contexts/EmployeeContext.js
+++ src/contexts/EmployeeContext.js
@@ -1,10 +1,10 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid"; //bu paket sayesinde "unıque ıd" üretimi yapabiliriz.
 
 export const EmployeeContext = createContext();
 
 const EmployeeContextProvider = (props) => {
-  const [employees,setEmployess] = useState([
+  const [employees,setEmployees] = useState([
     {
       id: uuidv4(),
       name: "Ömer Faruk",
@@ -42,18 +42,33 @@ const EmployeeContextProvider = (props) => {
     },
   ]);
 
+
+  //!Daha sonra objelerimizi getItem ile çağırdık.Ve statemizi obje formatında güncellemiş olduk.
+  useEffect(() => {
+    const employees=localStorage.getItem("employees")
+    setEmployees(JSON.parse(employees))
+  }, [])
+
+  //!Öncelikle elimizdeki objeleri setItem ile string olarak storage'e gömdük.
+  useEffect(() => {
+   localStorage.setItem("employees",JSON.stringify(employees))
+  })
+
+  
+  
+
   const sortedEmployess = employees.sort((a,b)=>a.name<b.name ? -1 : 1)
 
   const addEmployee =(name,email,address,phone)=>{
-    setEmployess([...employees,{id:uuidv4(),name,email,address,phone}])
+    setEmployees([...employees,{id:uuidv4(),name,email,address,phone}])
   }
 
   const deleteEmployee =(id)=>{
-    setEmployess(employees.filter(employee => employee.id !== id))
+    setEmployees(employees.filter(employee => employee.id !== id))
   }
 
   const updateEmployee = (id,updatedEmployee)=>{
-    setEmployess(employees.map((employee)=>(employee.id ===id ? updatedEmployee:employee)))
+    setEmployees(employees.map((employee)=>(employee.id ===id ? updatedEmployee:employee)))
   }
 
   return (
